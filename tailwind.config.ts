14aafee85b1173be6084f42c7ae63f243eb6aa28
James <thejamescodes@gmail.com>
2025-02-10 06:18:32 +0800

    Dark mode removed

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  media: false, // Disable dark mode
+  darkMode: false, 
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
